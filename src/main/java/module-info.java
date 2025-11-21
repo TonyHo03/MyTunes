9f00894047a2e128ecf1692be32b55a0a85ff299
ISMAIL\SNOOP <ismahm01@easv365.dk>
2025-11-21 12:02:33 +0100

    no message

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module dk.easv.mytunes {
 
     opens dk.easv.mytunes to javafx.fxml;
     exports dk.easv.mytunes;
+    exports dk.easv.mytunes.gui;
+    opens dk.easv.mytunes.gui to javafx.fxml;
 }
