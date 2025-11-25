5d31b19681ee67bb0f1d74504ba4bdd741a76e2b
ISMAIL\SNOOP <ismahm01@easv365.dk>
2025-11-25 07:15:49 +0100

    Færdiggjorde loadSongs og createSongs metoder i DAL-laget

--- src/main/java/dk/easv/mytunes/dal/MyTunesDAO_DB.java
+++ src/main/java/dk/easv/mytunes/dal/MyTunesDAO_DB.java
@@ -1,5 +1,7 @@
 package dk.easv.mytunes.dal;
 
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import dk.easv.mytunes.be.Playlist;
 import dk.easv.mytunes.be.Song;
 import dk.easv.mytunes.dal.db.DBConnector;
@@ -7,6 +9,7 @@ import dk.easv.mytunes.dal.db.DBConnector;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import java.io.IOException;
 import java.nio.file.Files;
 import com.mpatric.mp3agic.Mp3File;
 import java.io.File;
@@ -51,57 +54,91 @@ public class MyTunesDAO_DB implements IMyTunesDataAccess{
 
         File[] songs = songFolder.listFiles();
 
+        // Hvis der er ingen sange i mappen, så skal funktionen ikke køre.
         if (songs == null) {
             return;
         }
 
         try (Connection conn = dbConnector.getConnection()) {
 
-            Statement stmt = conn.createStatement();
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
             String sql = "SELECT * FROM dbo.Song";
             ResultSet rs = stmt.executeQuery(sql);
 
-            while (rs.next()) {
-
-                int Id = rs.getInt("Id");
-                String Title = rs.getString("Title");
-                String Artist = rs.getString("Artist");
-                String Category = rs.getString("Category");
-                Time Time = rs.getTime("Duration");
-                String file = rs.getString("FilePath");
+            // Tjekker om der er overhovedet er rækker i resultset. Hvis ikke, så er der ingen sange i databasen.
+            if (!rs.next()) {
+                System.out.println("Der er INGEN sange i databasen. Indsætter alle sange.");
 
                 for (File song: songs) {
-                    System.out.println("looping");
+                    String timeString = getDuration(song);
+                    Time time = java.sql.Time.valueOf(timeString);
+
+                    String tempTitle = song.getName().split("\\.")[0];
+
+                    createSong(tempTitle, "", "", time, song.getPath());
+                }
+
+                return;
+
+            } else {rs.beforeFirst();}
+
+            // Loop igennem alle sange for at tjekke om de allerede eksisterer i databasen.
+            for (File song: songs) {
+
+                int rowCount = 0;
+                int notFound = 0;
+
+                // Loop igennem resultset.
+                while (rs.next()) {
+                    rowCount++;
+
+                    int Id = rs.getInt("Id");
+                    String Title = rs.getString("Title");
+                    String Artist = rs.getString("Artist");
+                    String Category = rs.getString("Category");
+                    Time Time = rs.getTime("Duration");
+                    String file = rs.getString("FilePath");
+
+                    // Hvis sangen ikke har samme filsti som rækken i databasen, så tæller den "notFound" op.
                     if (!song.getPath().equals(file)) {
-                        System.out.println("New Song");
-
-                        Mp3File mp3File = new Mp3File("src/main/resources/dk/easv/mytunes/songs/" + song.getName());
-                        double duration = mp3File.getLengthInSeconds();
-
-                        int durationInSeconds = (int) duration;
-                        int hours = durationInSeconds / 3600;
-                        int minutes = (durationInSeconds % 3600) / 60;
-                        int seconds = durationInSeconds % 60;
-
-                        String timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
-                        Time time = java.sql.Time.valueOf(timeString);
-
-
-                        String tempTitle = song.getName().split("\\.")[0];
-                        createSong(tempTitle, "", "", time, song.getPath());
-                        break;
-
+                        notFound++;
                     }
                 }
+
+                // Hvis true, så betyder det at sangen ikke fandtes i alle rækker af resultset.
+                if (notFound == rowCount) {
+
+                    rowCount = 0;
+                    notFound = 0;
+                    System.out.println("Sangen er ikke i databasen: " + rowCount + " " + notFound);
+
+                    Time time = Time.valueOf(getDuration(song));
+                    String tempTitle = song.getName().split("\\.")[0];
+
+                    createSong(tempTitle, "", "", time, song.getPath());
+                }
+
+                // Resetter resultset, så den viser den første række igen.
+                rs.beforeFirst();
             }
         } catch (SQLException e) {
 
             e.printStackTrace();
             throw new Exception("Could not load songs.");
-
         }
+    }
 
+    private static String getDuration(File song) throws IOException, UnsupportedTagException, InvalidDataException {
+        Mp3File mp3File = new Mp3File("src/main/resources/dk/easv/mytunes/songs/" + song.getName());
+        double duration = mp3File.getLengthInSeconds();
+
+        int durationInSeconds = (int) duration;
+        int hours = durationInSeconds / 3600;
+        int minutes = (durationInSeconds % 3600) / 60;
+        int seconds = durationInSeconds % 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 
     @Override
